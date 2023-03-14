6037cce3d17608dd5c92d8ac746f2893c0891cdb
Chien. Nguyen Minh (3) <chiennm3@vng.com.vn>
2023-03-14 20:22:09 +0700

    Add integration test for GetPatronProfile

--- internal/lending/adapters/postgres_patron_repository_test.go
+++ internal/lending/adapters/postgres_patron_repository_test.go
@@ -17,6 +17,7 @@ import (
 	"github.com/chiennguyen196/go-library/internal/common/database"
 	"github.com/chiennguyen196/go-library/internal/lending/adapters"
 	"github.com/chiennguyen196/go-library/internal/lending/adapters/models"
+	"github.com/chiennguyen196/go-library/internal/lending/app/query"
 	"github.com/chiennguyen196/go-library/internal/lending/domain"
 )
 
@@ -109,4 +110,62 @@ func TestPostgresPatronRepository_UpdateWithBook(t *testing.T) {
 
 	assertPersistedPatronEquals(t, repo, updatedPatron)
 	assertPersistedBookEquals(t, adapters.NewPostgresBookRepository(db), updatedBook)
+}
+
+func TestPostgresPatronRepository_GetPatronProfile(t *testing.T) {
+	db := database.NewSqlDB()
+	t.Cleanup(func() {
+		_ = db.Close()
+	})
+	ctx := context.Background()
+
+	repo := adapters.NewPostgresPatronRepository(db)
+	dbPatron := addExamplePatron(t, db)
+	dbBook1 := addExampleAvailableBook(t, db)
+	holdDuration, err := domain.NewHoldDuration(time.Now(), 5)
+	require.NoError(t, err)
+
+	err = repo.UpdateWithBook(ctx, domain.PatronID(dbPatron.ID), domain.BookID(dbBook1.ID), func(ctx context.Context, patron *domain.Patron, book *domain.Book) error {
+		err = patron.PlaceOnHold(book.BookInfo(), holdDuration)
+		require.NoError(t, err)
+
+		err = book.HoldBy(patron.ID(), holdDuration)
+		require.NoError(t, err)
+
+		return nil
+	})
+
+	expectedQueryPatronProfile := query.PatronProfile{
+		PatronID:   dbPatron.ID,
+		PatronType: domain.PatronTypeRegular,
+		Holds: []domain.Hold{
+			{
+				BookID:       domain.BookID(dbBook1.ID),
+				PlacedAt:     domain.LibraryBranchID(dbBook1.LibraryBranchID),
+				HoldDuration: holdDuration,
+			},
+		},
+		CheckedOuts:      []query.CheckedOut{},
+		OverdueCheckouts: []query.OverdueCheckout{},
+	}
+	require.NoError(t, err)
+
+	actual, err := repo.GetPatronProfile(ctx, domain.PatronID(dbPatron.ID))
+	require.NoError(t, err)
+
+	cmpOpts := []cmp.Option{
+		cmp.AllowUnexported(
+			time.Time{},
+			domain.PatronType{},
+			domain.Patron{},
+			domain.Hold{},
+			domain.HoldDuration{},
+			domain.Book{},
+		),
+	}
+
+	assert.True(t,
+		cmp.Equal(expectedQueryPatronProfile, actual, cmpOpts...),
+		cmp.Diff(expectedQueryPatronProfile, actual, cmpOpts...),
+	)
 }
